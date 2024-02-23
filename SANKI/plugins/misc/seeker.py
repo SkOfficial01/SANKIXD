5c28c71ce71017c1982d3e4574b6396b1dc26319
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-23 18:16:58 +0530

    Update seeker.py

--- SANKI/plugins/misc/seeker.py
+++ SANKI/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from AarohiX.misc import db
-from AarohiX.utils.database import get_active_chats, is_music_playing
+from SANKI.misc import db
+from SANKI.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
