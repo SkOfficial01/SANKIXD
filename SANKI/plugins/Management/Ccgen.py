e1cb58e5782fa40d17c94cb553053f592786b75a
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-23 18:53:18 +0530

    Update Ccgen.py

--- SANKI/plugins/Management/Ccgen.py
+++ SANKI/plugins/Management/Ccgen.py
@@ -1,5 +1,5 @@
 from SafoneAPI import SafoneAPI
-from AarohiX import app
+from SANKI import app
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 api = SafoneAPI()
