e3436480ab090c937835e009eef853dfb8b9f277
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-23 18:32:13 +0530

    Update dev.py

--- SANKI/plugins/tools/dev.py
+++ SANKI/plugins/tools/dev.py
@@ -10,7 +10,7 @@ from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from AarohiX import app
+from SANKI import app
 from config import OWNER_ID
 from Jinx import RUN
 
