f6c51af279542d2739a2a8128057ee8f1dfecae0
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-23 22:10:25 +0530

    Update Pretender.py

--- SANKI/plugins/Management/Pretender.py
+++ SANKI/plugins/Management/Pretender.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from AarohiX.utils.Databases.pretenderdb import impo_off, impo_on, check_pretender, add_userdata, get_userdata, usr_data
-from AarohiX import app
+from SANKI.utils.Databases.pretenderdb import impo_off, impo_on, check_pretender, add_userdata, get_userdata, usr_data
+from SANKI import app
 
 
 
