4f28af87d2495b059866e41b0dbff05f4836c98e
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-23 22:05:31 +0530

    Update PasswordGen.py

--- SANKI/plugins/Management/PasswordGen.py
+++ SANKI/plugins/Management/PasswordGen.py
@@ -1,6 +1,6 @@
 import random, os
 from pyrogram import Client, filters, enums 
-from AarohiX import app
+from SANKI import app
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
