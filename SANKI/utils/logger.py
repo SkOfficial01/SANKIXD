4a7c08fb83285b610bf7bf9262c7d625bdec9e18
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-23 23:35:54 +0530

    Update logger.py

--- SANKI/utils/logger.py
+++ SANKI/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from AarohiX import app
-from AarohiX.utils.database import is_on_off
+from SANKI import app
+from SANKI.utils.database import is_on_off
 from config import LOGGER_ID
 
 
