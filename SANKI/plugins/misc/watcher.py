a37b4f767e289eb1c449d57a379e69109e59bb62
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-24 23:50:20 +0530

    Update watcher.py

--- SANKI/plugins/misc/watcher.py
+++ SANKI/plugins/misc/watcher.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from SANKI import app
-from SANKI.core.call import Dil
+from SANKI.core.call import SANKI
 
 welcome = 20
 close = 30
