714dddb22b7963eb29fefaf0227581420098f5a2
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-25 00:30:30 +0530

    Update userbot.py

--- SANKI/core/userbot.py
+++ SANKI/core/userbot.py
@@ -54,8 +54,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("LOVE_FEELINGS_WILL1")
-                await self.one.join_chat("LOVE_FEELINGS_WILL1")
+                await self.one.join_chat("sankixdlogger")
+                await self.one.join_chat("sankixdlogger")
             except:
                 pass
             assistants.append(1)
@@ -83,8 +83,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("LOVE_FEELINGS_WILL1")
-                await self.two.join_chat("LOVE_FEELINGS_WILL1")
+                await self.two.join_chat("sankixdlogger")
+                await self.two.join_chat("sankixdlogger")
             except:
                 pass
             assistants.append(2)
@@ -112,8 +112,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("LOVE_FEELINGS_WILL1")
-                await self.three.join_chat("LOVE_FEELINGS_WILL1")
+                await self.three.join_chat("sankixdlogger")
+                await self.three.join_chat("sankixdlogger")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("LOVE_FEELINGS_WILL1")
-                await self.four.join_chat("LOVE_FEELINGS_WILL1")
+                await self.four.join_chat("sankixdlogger")
+                await self.four.join_chat("sankixdlogger")
             except:
                 pass
             assistants.append(4)
@@ -170,8 +170,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("LOVE_FEELINGS_WILL1")
-                await self.five.join_chat("LOVE_FEELINGS_WILL1")
+                await self.five.join_chat("sankixdlogger")
+                await self.five.join_chat("sankixdlogger")
             except:
                 pass
             assistants.append(5)
