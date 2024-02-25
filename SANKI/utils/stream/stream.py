56d524ed6ad5d0eb6211c9d90fc85f3c22dc2efe
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-25 09:57:47 +0530

    Update stream.py

--- SANKI/utils/stream/stream.py
+++ SANKI/utils/stream/stream.py
@@ -79,7 +79,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                await Dil.join_call(
+                await SANKI.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -165,7 +165,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Dil.join_call(
+            await SANKI.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -225,7 +225,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Dil.join_call(chat_id, original_chat_id, file_path, video=None)
+            await SANKI.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -277,7 +277,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Dil.join_call(chat_id, original_chat_id, file_path, video=status)
+            await SANKI.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -333,7 +333,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Dil.join_call(
+            await SANKI.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Dil.join_call(
+            await SANKI.join_call(
                 chat_id,
                 original_chat_id,
                 link,
